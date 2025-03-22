661082b73622b85edf3b6405b5abbb37c76c084a
elezkadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2025-03-22 04:10:47 +0100

    fix localhost

--- Front_Storehouse/vite.config.mjs
+++ Front_Storehouse/vite.config.mjs
@@ -1,47 +1,24 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = `${env.VITE_APP_BASE_NAME}`;
-  const PORT = `${'3000'}`;
-
   return {
     server: {
-      // this ensures that the browser opens upon server start
-      open: true,
-      // this sets a default port to 3000
-      port: PORT
+      host: 'localhost',
+      port: 5173,
+      open: true
     },
     define: {
       global: 'window'
     },
     resolve: {
-      alias: [
-        // { find: '', replacement: path.resolve(__dirname, 'src') },
-        // {
-        //   find: /^~(.+)/,
-        //   replacement: path.join(process.cwd(), 'node_modules/$1')
-        // },
-        // {
-        //   find: /^src(.+)/,
-        //   replacement: path.join(process.cwd(), 'src/$1')
-        // }
-        // {
-        //   find: 'assets',
-        //   replacement: path.join(process.cwd(), 'src/assets')
-        // },
-      ]
+      alias: []
     },
     css: {
       preprocessorOptions: {
-        scss: {
-          charset: false
-        },
-        less: {
-          charset: false
-        }
+        scss: { charset: false },
+        less: { charset: false }
       },
       charset: false,
       postcss: {
@@ -59,7 +36,7 @@ export default defineConfig(({ mode }) => {
         ]
       }
     },
-    base: API_URL,
+    base: '/', // ✅ Set base to '/' to remove the long path
     plugins: [react(), jsconfigPaths()]
   };
 });
