a66e18b4a81963e890cac3eee22d745b54352c34
elezkadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2025-03-23 00:02:47 +0100

    Creating Users

--- Storehouse_Management/Infrastructure/Data/AppDbContext.cs
+++ Storehouse_Management/Infrastructure/Data/AppDbContext.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
    
