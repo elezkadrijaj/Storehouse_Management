0c0a2e4d29e3ecdf5a8ee0003e7794893780bb9d
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2025-03-25 00:54:51 +0100

    Company Table
    Krijimi i entitetit te Company dhe konfigurimit te saj.
    
    Co-Authored-By: elezkadrijaj <150354782+elezkadrijaj@users.noreply.github.com>

--- Storehouse_Management/Infrastructure/Data/AppDbContext.cs
+++ Storehouse_Management/Infrastructure/Data/AppDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Core.Configurations;
 
 namespace Infrastructure.Data
 {
     public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-   
 
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new CompanyConfigurations());
+
+            builder.ApplyConfiguration(new ApplicationUserConfigurations());
+        }
     }
 }
