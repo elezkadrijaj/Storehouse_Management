efee3dc655a0d84e85c6d59c32a7251210608810
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2025-03-25 00:57:08 +0100

    Company & User
    Lidhja e company me user.
    
    Co-Authored-By: elezkadrijaj <150354782+elezkadrijaj@users.noreply.github.com>

---
+++ Storehouse_Management/Core/Configurations/ApplicationUserConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Configurations
+{
+    public class ApplicationUserConfigurations : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasOne(e => e.Companies)
+                .WithMany()
+                .HasForeignKey(e => e.CompaniesId);
+        }
+    }
+}
