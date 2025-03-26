7a4ecb2c95e7242daafb2e4b97d68b01e6e84836
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2025-03-26 22:52:41 +0100

    Product Entity

---
+++ Storehouse_Management/Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int ProductId {  get; set; }
+        public string Name { get; set; }
+        public string Stock {  get; set; }
+        public DateTime ExpiryDate {  get; set; }
+        public double Price {  get; set; }
+
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }//foreign key
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }//foreign key
+
+    }
+}
