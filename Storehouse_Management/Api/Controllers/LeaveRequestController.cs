e1440b5cbfdb84febb1cac8e6d9e1f8e47b817fc
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2025-03-28 01:26:22 +0100

    LeaveRequest Entity and Controller

---
+++ Storehouse_Management/Api/Controllers/LeaveRequestController.cs
@@ -0,0 +1,61 @@
+﻿using Application.DTOs;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveRequestController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LeaveRequestController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LeaveRequest>>> GetRequests()
+        {
+            return await _context.LeaveRequest.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveRequest>> GetRequest(int id)
+        {
+            var request = await _context.LeaveRequest.FindAsync(id);
+
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return request;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LeaveRequest>> CreateRequest(LeaveRequestDto requestDto)
+        {
+            
+            var leaveRequest = new LeaveRequest
+            {
+                UserId = requestDto.UserId,
+                StartDate = requestDto.StartDate,
+                EndDate = requestDto.EndDate,
+                Status = requestDto.Status,
+                ManagerId = requestDto.ManagerId
+                
+            };
+
+            _context.LeaveRequest.Add(leaveRequest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRequest), new { id = leaveRequest.LeaveRequestId }, leaveRequest);
+        }
+
+    }
+}
