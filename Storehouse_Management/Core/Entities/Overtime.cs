0d19b0a4de319e2c9b262479d644204ebec37c7c
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2025-03-28 02:00:07 +0100

    Overtime Entity and Controller

---
+++ Storehouse_Management/Core/Entities/Overtime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Overtime
+    {
+        public int OvertimeId {  get; set; }
+        public string UserId {  get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime Date { get; set; }
+        public double HoursWorked {  get; set; }
+       
+
+
+    }
+}
