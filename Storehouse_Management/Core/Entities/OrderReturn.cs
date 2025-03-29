9e233258a293bce93c993ac6ef81832780e876da
ArianitKadrijaj <204411359+ArianitKadrijaj21@users.noreply.github.com>
2025-03-29 02:00:51 +0100

    Order Entities and COnfiguration

---
+++ Storehouse_Management/Core/Entities/OrderReturn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class OrderReturn
+    {
+        public int OrderReturnId { get; set; }
+        public string Reason { get; set; }
+        public string Status { get; set; }
+
+        public int? OrderId { get; set; }
+        public Order Orders { get; set; }
+    }
+}
