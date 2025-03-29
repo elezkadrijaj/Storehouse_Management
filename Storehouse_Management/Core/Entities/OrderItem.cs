55570ed0b1d30e6c3608d453bb23930212fbd390
Shkemb <hasanmetajshkemb@gmail.com>
2025-03-29 01:07:16 +0100

    Creating Order and Order Item

---
+++ Storehouse_Management/Core/Entities/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class OrderItem
+    {
+        public int OrderItemId {  get; set; }
+        public int Quantity {get; set; }
+        public float Price { get; set; }
+        
+        public int? OrdersId { get; set; }
+
+        public Order? Orders { get; set; }
+
+        public string? ProductsId {  get; set; }
+
+        public Product? Products { get; set; }
+}
+    }
