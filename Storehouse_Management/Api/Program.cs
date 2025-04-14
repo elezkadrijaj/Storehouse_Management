d46497bd855d1e50180ffce2ed2c8371bd96f2d3
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2025-04-14 22:35:03 +0200

    Product Api Handling
    Thirrja e Api's ne React App, rregullimi i backend per shfaqje te images dhe pastrimi i kodit.

--- Storehouse_Management/Api/Program.cs
+++ Storehouse_Management/Api/Program.cs
@@ -14,6 +14,7 @@ using Swashbuckle.AspNetCore.Filters;
 using Application.Interfaces;
 using Application.Services.Products;
 using Application.Services.Orders;
+using Microsoft.Extensions.FileProviders;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -125,6 +126,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "images")),
+    RequestPath = "/images"
+});
 
 if (app.Environment.IsDevelopment())
 {
@@ -135,6 +142,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("AllowReactApp");
+
+app.UseStaticFiles();
 
 app.UseAuthentication();
 
