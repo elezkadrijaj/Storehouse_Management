fe190450fcc9c8e5f6f9d5f9f00d7ad3262b8a10
elezkadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2025-04-15 03:23:50 +0200

    Set up User profile information and logut
    Co-Authored-By: ArianitKadrijaj21 <204411359+ArianitKadrijaj21@users.noreply.github.com>
    Co-Authored-By: Shkemb <139810707+ShkembH@users.noreply.github.com>

---
+++ Storehouse_Management/Application/DTOs/UserProfileDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class UserProfileDto
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public IList<string> Roles { get; set; }
+        public int? CompaniesId { get; set; } 
+        public string CompanyName { get; set; }
+        public string CompanyBusinessNumber { get; set; }
+        public int? StorehouseId { get; set; }
+        public string StorehouseName { get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}
