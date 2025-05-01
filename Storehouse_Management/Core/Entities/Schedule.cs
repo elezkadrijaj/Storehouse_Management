3224470a24ad1161b1f29ddd5a6f8131405a675f
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2025-05-02 00:00:51 +0200

    Update Schedule table

--- Storehouse_Management/Core/Entities/Schedule.cs
+++ Storehouse_Management/Core/Entities/Schedule.cs
@@ -11,9 +11,9 @@ namespace Core.Entities
         public int ScheduleId {  get; set; }
         public string UserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public DateTime BreakTime { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string BreakTime { get; set; }
 
     }
 }
---
+++ Storehouse_Management/Infrastructure/Migrations/20250413000959_scheduleupdate.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class scheduleupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StartDate",
+                table: "Schedule",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndDate",
+                table: "Schedule",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BreakTime",
+                table: "Schedule",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Schedule",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Schedule",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BreakTime",
+                table: "Schedule",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValueSql: "GETDATE()");
+        }
+    }
+}
